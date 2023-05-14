3104955742a0d193833ac6eca50169e79c3f984f
Jsmond2016 <Jsmond2016@gmail.com>
2023-05-14 10:07:42 +0800

    feat: 定义兼容类型，兼容 less 和图片资源

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -6,4 +6,9 @@ declare module "*.module.scss" {
   export default classes
 }
 
+declare module "*.module.less" {
+  const classes: { readonly [key: string]: string }
+  export default classes
+}
+
 declare module "*.ts"
