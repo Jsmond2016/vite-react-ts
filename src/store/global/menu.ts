79791b5e940e433e7969f1a9f00b9dc1ac425280
Jsmond2016 <jsmond2016@gmail.com>
2024-10-07 12:14:55 +0800

    🐛fix(顶部工具栏模块): 修复菜单模块使用 persist 中间件页面刷新报错问题

--- src/store/global/menu.ts
+++ src/store/global/menu.ts
@@ -1,6 +1,6 @@
 import { last, prop } from 'ramda';
 import { create } from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+// import { devtools, persist } from 'zustand/middleware';
 
 type IActions = {
   setIsMenuCollapsed: () => void;
@@ -21,52 +21,55 @@ const initState = {
 };
 
 export const useMenuStore = create<IActions & typeof initState>()(
-  devtools(
-    persist(
-      (set) => ({
-        ...initState,
-        setIsMenuCollapsed: () => set((state) => ({ isMenuCollapsed: !state.isMenuCollapsed })),
-        setMenuList: (_menuList: any[]) => set(() => ({ menuList: _menuList })),
-        setOpenedMenuKeys: (_openedMenuKeys: string[]) =>
-          set(() => {
-            return { openedMenuKeys: _openedMenuKeys };
-          }),
-        setCurOpenedMenuItems: (curMenuItems) =>
-          set((state) => {
-            const { openedPageTabs } = state;
-            let menuItemsState: any = { curOpenedMenuItems: curMenuItems };
-            const openedTabKeys = openedPageTabs.map(prop('key'));
-            const lastMenuItem: any = last(curMenuItems);
-            if (lastMenuItem) {
-              menuItemsState = {
-                ...menuItemsState,
-                curTabKey: lastMenuItem?.key,
-              };
-              if (!openedTabKeys.includes(lastMenuItem?.key)) {
-                menuItemsState = {
-                  ...menuItemsState,
-                  openedPageTabs: [...openedPageTabs, lastMenuItem].filter((v) => v.isAccessed),
-                };
-              }
-            }
-            return menuItemsState;
-          }),
-        setOpenedPageTabs: (newWorkTabs: any[]) =>
-          set(() => {
-            if (newWorkTabs.length === 0) {
-              return {
-                openedPageTabs: [],
-                curTabKey: undefined,
-              };
-            }
-            const tabKey = last(newWorkTabs).key;
-            return { openedPageTabs: newWorkTabs, curTabKey: tabKey };
-          }),
-        setCurTabKey: (tabKey: string) => set(() => ({ curTabKey: tabKey })),
+  // devtools(
+  // FIXME: bug， 这里使用后，重启项目打开页面报错
+  // GPT: 确保你存储在 Zustand 状态中的数据是可以被 React 渲染的。避免将复杂的对象（如 React 组件、DOM 节点等）存储在状态中。
+  // 如果问题依然存在，尝试清除 localStorage，看看是否是因为存储了旧数据导致的问题。
+  // persist(
+  (set) => ({
+    ...initState,
+    setIsMenuCollapsed: () => set((state) => ({ isMenuCollapsed: !state.isMenuCollapsed })),
+    setMenuList: (_menuList: any[]) => set(() => ({ menuList: _menuList })),
+    setOpenedMenuKeys: (_openedMenuKeys: string[]) =>
+      set(() => {
+        return { openedMenuKeys: _openedMenuKeys };
       }),
-      {
-        name: 'menuConfig',
-      },
-    ),
-  ),
+    setCurOpenedMenuItems: (curMenuItems) =>
+      set((state) => {
+        const { openedPageTabs } = state;
+        let menuItemsState: any = { curOpenedMenuItems: curMenuItems };
+        const openedTabKeys = openedPageTabs.map(prop('key'));
+        const lastMenuItem: any = last(curMenuItems);
+        if (lastMenuItem) {
+          menuItemsState = {
+            ...menuItemsState,
+            curTabKey: lastMenuItem?.key,
+          };
+          if (!openedTabKeys.includes(lastMenuItem?.key)) {
+            menuItemsState = {
+              ...menuItemsState,
+              openedPageTabs: [...openedPageTabs, lastMenuItem].filter((v) => v.isAccessed),
+            };
+          }
+        }
+        return menuItemsState;
+      }),
+    setOpenedPageTabs: (newWorkTabs: any[]) =>
+      set(() => {
+        if (newWorkTabs.length === 0) {
+          return {
+            openedPageTabs: [],
+            curTabKey: undefined,
+          };
+        }
+        const tabKey = last(newWorkTabs).key;
+        return { openedPageTabs: newWorkTabs, curTabKey: tabKey };
+      }),
+    setCurTabKey: (tabKey: string) => set(() => ({ curTabKey: tabKey })),
+  }),
+  //     {
+  //       name: 'menuConfig',
+  //     },
+  //   ),
+  // ),
 );
--- src/views/demos/demo-1/index.tsx
+++ src/views/demos/demo-1/index.tsx
@@ -1,105 +1,107 @@
-import { Button, Modal } from 'antd';
-import React, { useRef, useState } from 'react';
-import type { DraggableData, DraggableEvent } from 'react-draggable';
-import Draggable from 'react-draggable';
+// import { Button, Modal } from 'antd';
+// import React, { useRef, useState } from 'react';
+// import type { DraggableData, DraggableEvent } from 'react-draggable';
+// import Draggable from 'react-draggable';
 
-import styles from './index.module.less';
+// import styles from './index.module.less';
 
-const Index: React.FC = () => {
-  const [open, setOpen] = useState(false);
-  const [disabled, setDisabled] = useState(true);
-  const [bounds, setBounds] = useState({
-    left: 0,
-    top: 0,
-    bottom: 0,
-    right: 0,
-  });
-  const draggleRef = useRef<HTMLDivElement>(null);
+// const Index: React.FC = () => {
+//   const [open, setOpen] = useState(false);
+//   const [disabled, setDisabled] = useState(true);
+//   const [bounds, setBounds] = useState({
+//     left: 0,
+//     top: 0,
+//     bottom: 0,
+//     right: 0,
+//   });
+//   const draggleRef = useRef<HTMLDivElement>(null);
 
-  const showModal = () => {
-    setOpen(true);
-  };
+//   const showModal = () => {
+//     setOpen(true);
+//   };
 
-  // const handleOk = (e: React.MouseEvent<HTMLElement>) => {
-  //   console.log(e);
-  //   setOpen(false);
-  // };
+//   // const handleOk = (e: React.MouseEvent<HTMLElement>) => {
+//   //   console.log(e);
+//   //   setOpen(false);
+//   // };
 
-  const handleCancel = (e: React.MouseEvent<HTMLElement>) => {
-    console.log(e);
-    setOpen(false);
-  };
+//   const handleCancel = (e: React.MouseEvent<HTMLElement>) => {
+//     console.log(e);
+//     setOpen(false);
+//   };
 
-  const onStart = (_event: DraggableEvent, uiData: DraggableData) => {
-    const { clientWidth, clientHeight } = window.document.documentElement;
-    const targetRect = draggleRef.current?.getBoundingClientRect();
-    if (!targetRect) {
-      return;
-    }
-    setBounds({
-      left: -targetRect.left + uiData.x,
-      right: clientWidth - (targetRect.right - uiData.x),
-      top: -targetRect.top + uiData.y,
-      bottom: clientHeight - (targetRect.bottom - uiData.y),
-    });
-  };
+//   const onStart = (_event: DraggableEvent, uiData: DraggableData) => {
+//     const { clientWidth, clientHeight } = window.document.documentElement;
+//     const targetRect = draggleRef.current?.getBoundingClientRect();
+//     if (!targetRect) {
+//       return;
+//     }
+//     setBounds({
+//       left: -targetRect.left + uiData.x,
+//       right: clientWidth - (targetRect.right - uiData.x),
+//       top: -targetRect.top + uiData.y,
+//       bottom: clientHeight - (targetRect.bottom - uiData.y),
+//     });
+//   };
 
-  return (
-    <div className={styles['demo-1-container']}>
-      <Button onClick={showModal}>Open Draggable Modal</Button>
-      <div id="modal-container"></div>
-      <Modal
-        getContainer="#modal-container"
-        mask={false}
-        title={
-          <div
-            style={{
-              width: '100%',
-              cursor: 'move',
-              border: '1px solid red',
-              padding: '16px 16px',
-            }}
-            onMouseOver={() => {
-              if (disabled) {
-                setDisabled(false);
-              }
-            }}
-            onMouseOut={() => {
-              setDisabled(true);
-            }}
-            // fix eslintjsx-a11y/mouse-events-have-key-events
-            // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
-            onFocus={() => {}}
-            onBlur={() => {}}
-            // end
-          >
-            拖拽句柄区域
-          </div>
-        }
-        open={open}
-        // onOk={handleOk}
-        footer={null}
-        onCancel={handleCancel}
-        modalRender={(modal) => (
-          <Draggable
-            disabled={disabled}
-            bounds={bounds}
-            onStart={(event, uiData) => onStart(event, uiData)}
-          >
-            <div ref={draggleRef}>{modal}</div>
-          </Draggable>
-        )}
-      >
-        <p>
-          Just don&apos;t learn physics at school and your life will be full of magic and miracles.
-        </p>
-        <br />
-        <p>Day before yesterday I saw a rabbit, and yesterday a deer, and today, you.</p>
-        <img src="https://source.unsplash.com/random/200x200?sig=3" alt="img1" />
-        {/* https://stackoverflow.com/questions/60797390/generate-random-image-by-url */}
-      </Modal>
-    </div>
-  );
-};
+//   return (
+//     <div className={styles['demo-1-container']}>
+//       <Button onClick={showModal}>Open Draggable Modal</Button>
+//       <div id="modal-container"></div>
+//       <Modal
+//         getContainer="#modal-container"
+//         mask={false}
+//         title={
+//           <div
+//             style={{
+//               width: '100%',
+//               cursor: 'move',
+//               border: '1px solid red',
+//               padding: '16px 16px',
+//             }}
+//             onMouseOver={() => {
+//               if (disabled) {
+//                 setDisabled(false);
+//               }
+//             }}
+//             onMouseOut={() => {
+//               setDisabled(true);
+//             }}
+//             // fix eslintjsx-a11y/mouse-events-have-key-events
+//             // https://github.com/jsx-eslint/eslint-plugin-jsx-a11y/blob/master/docs/rules/mouse-events-have-key-events.md
+//             onFocus={() => {}}
+//             onBlur={() => {}}
+//             // end
+//           >
+//             拖拽句柄区域
+//           </div>
+//         }
+//         open={open}
+//         // onOk={handleOk}
+//         footer={null}
+//         onCancel={handleCancel}
+//         modalRender={(modal) => (
+//           <Draggable
+//             disabled={disabled}
+//             bounds={bounds}
+//             onStart={(event, uiData) => onStart(event, uiData)}
+//           >
+//             <div ref={draggleRef}>{modal}</div>
+//           </Draggable>
+//         )}
+//       >
+//         <p>
+//           Just don&apos;t learn physics at school and your life will be full of magic and miracles.
+//         </p>
+//         <br />
+//         <p>Day before yesterday I saw a rabbit, and yesterday a deer, and today, you.</p>
+//         <img src="https://source.unsplash.com/random/200x200?sig=3" alt="img1" />
+//         {/* https://stackoverflow.com/questions/60797390/generate-random-image-by-url */}
+//       </Modal>
+//     </div>
+//   );
+// };
+
+const Index = () => <h1>Test Index </h1>;
 
 export default Index;
